045fd41bdc7a875f0a6fcc3aa633d71088b3862d
Ademir Candelario R <ademircandelario07@hotmail.com>
2021-07-25 21:33:13 -0600

    adding close menu nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,28 @@ const navMenu = document.getElementById('nav-menu'),
 /* Validate if constant exists */
 if(navToggle){
     navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
+        navMenu.classList.add('show-menu');
     })
 }
 
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if (navClose) {
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    //When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 
