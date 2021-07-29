eac925b168ce829fb36c836ef6390d5d9fbf4d92
Ademir Candelario R <ademircandelario07@hotmail.com>
2021-07-28 21:30:15 -0600

    first part of js services section

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,7 +70,13 @@ tabs.forEach(tab => {
 });
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close');
 
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
